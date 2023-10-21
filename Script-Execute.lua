fcc0808b1425e8881108e38530be48b50620be15
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2023-10-21 13:52:55 +0400

    Update Script-Execute.lua

--- Script-Execute.lua
+++ Script-Execute.lua
@@ -4,4 +4,3 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ahmadsgamer2/Dragon-H
 elseif  game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ahmadsgamer2/Dragon-Hub/main/Blox-Fruit.lua", true))()
 end
-end
