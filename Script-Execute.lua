b146d6285d0dd0b77b8c879296e767c312a05078
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2023-10-21 13:51:59 +0400

    Update Script-Execute.lua

--- Script-Execute.lua
+++ Script-Execute.lua
@@ -1,4 +1,7 @@
 print(game.PlaceId)
 if game.PlaceId == 3101667897 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ahmadsgamer2/Dragon-Hub/main/Legends-Of-Speed.lua", true))()
+elseif  game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ahmadsgamer2/Dragon-Hub/main/Blox-Fruit.lua", true))()
+end
 end
