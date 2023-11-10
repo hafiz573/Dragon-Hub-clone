c5a33fc27c8728fd2ee2524b7211f0b0385359fe
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2023-11-10 12:29:36 +0400

    Update Dragon-Hub-Ui.lua

--- Dragon-Hub-Ui.lua
+++ Dragon-Hub-Ui.lua
@@ -190,7 +190,7 @@ local Logo = Instance.new("ImageLabel")
 	Logo.BackgroundTransparency = 1.000
 	Logo.Position = UDim2.new(0, 2, 0, 22)
 	Logo.Size = UDim2.new(0, 140, 0, 140)
-	Logo.Image = "http://www.roblox.com/asset/?id=12438642669"
+	Logo.Image = "http://www.roblox.com/asset/?id=15306889718"
 local Tab = Instance.new("Frame")
 Tab.Name = "Tab"
 Tab.Parent = Main
